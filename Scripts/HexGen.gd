894346fb9b36f455a75ab291209b63500e2d0aa3
Néstor <nestorac@gmail.com>
2022-09-03 17:26:48 +0200

    Generate hexagon.

---
+++ Scripts/HexGen.gd
@@ -0,0 +1,37 @@
+extends Spatial
+
+var tmpMesh = Mesh.new()
+var vertices = PoolVector3Array()
+var UVs = PoolVector2Array()
+var mat = SpatialMaterial.new()
+export var color = Color(0.9, 0.1, 0.1)
+
+
+func _ready():
+	vertices.push_back(Vector3(2,0,0))
+	vertices.push_back(Vector3(1,0,-2))
+	vertices.push_back(Vector3(-1,0,-2))
+	vertices.push_back(Vector3(-2,0,0))
+	vertices.push_back(Vector3(-1,0,2))
+	vertices.push_back(Vector3(1,0,2))
+
+	UVs.push_back(Vector2(0,0))
+	UVs.push_back(Vector2(0,1))
+	UVs.push_back(Vector2(1,1))
+	UVs.push_back(Vector2(1,0))
+	UVs.push_back(Vector2(1,0))
+	UVs.push_back(Vector2(1,0))
+
+	mat.albedo_color = color
+
+	var st = SurfaceTool.new()
+	st.begin(Mesh.PRIMITIVE_TRIANGLE_FAN)
+	st.set_material(mat)
+
+	for v in vertices.size(): 
+		st.add_color(color)
+		st.add_uv(UVs[v])
+		st.add_vertex(vertices[v])
+
+	st.commit(tmpMesh)
+	$"../MeshInstance3".mesh = tmpMesh
