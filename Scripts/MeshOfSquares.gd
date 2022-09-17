a8bd2e9099996baf935c25c855b6783a6c60ea0c
Néstor <nestorac@gmail.com>
2022-09-17 17:14:44 +0200

    Trying the array...

--- Scripts/MeshOfSquares.gd
+++ Scripts/MeshOfSquares.gd
@@ -4,11 +4,40 @@ var square = preload("res://Scenes/Square.tscn")
 export var width = 10
 export var height = 5
 
+var squares = [width]
+
 func _ready():
 	var square_instance
 	
-	for i in width:
-		for j in height:
+	var up = 0
+	var down = 0
+	var right = 0
+	var left = 0
+	
+	for i in range(width):
+		for j in range(height):
 			square_instance = square.instance()
+			squares[i].append(square.instance())
 			add_child(square_instance)
 			square_instance.transform.origin = Vector3(i,0,j)
+	
+	
+	for i in range(width):
+		for j in range(height):
+			if (i > 0):
+				up = squares[i-1][j]
+			else:
+				up = 0
+			if (i < height - 1):
+				down = squares[i+1][j]
+			else:
+				down = 0
+			if (j > 0):
+				left = squares[i][j-1]
+			else:
+				left = 0
+			if (j < width -1):
+				right = squares[i][j+1]
+			else:
+				right = 0
+			square_instance.set_neighbors(up, down, left, right)
--- Scripts/Square.gd
+++ Scripts/Square.gd
@@ -2,11 +2,45 @@ extends MeshInstance
 
 class_name Square
 
+enum {WATER, LAND, ICE, GRASS}
+
+
 var tmpMesh = Mesh.new()
 var vertices = PoolVector3Array()
 var UVs = PoolVector2Array()
 var mat = SpatialMaterial.new()
-export var color = Color(0.9, 0.1, 0.1)
+var color = Color(0.5, 0, 0)
+export var terrain = WATER
+
+var up = 0
+var down = 0
+var left = 0
+var right = 0
+
+func set_neighbors(_up, _down, _left, _right):
+	if (_up != 0):
+		up = _up 
+	if (_down != 0):
+		down = _down
+	if (_left != 0):
+		left = _left
+	if (_right != 0):
+		right = _right
+
+func set_terrain(terrain):
+	
+	terrain = randi()%4
+	
+	match terrain:
+		WATER:
+			color = Color(0.1, 0.1, 1)
+		LAND:
+			color = Color(1, 0.66, 0)
+		ICE:
+			color = Color(0.15, 1, 1)
+		GRASS:
+			color = Color(0.1, 0.7, 0.1)
+	mat.albedo_color = color
 
 
 func load_fig():
@@ -28,6 +62,8 @@ func init():
 	st.begin(Mesh.PRIMITIVE_TRIANGLE_FAN)
 	st.set_material(mat)
 
+	set_terrain(WATER)
+
 	for v in vertices.size(): 
 		st.add_color(color)
 		st.add_uv(UVs[v])
@@ -41,7 +77,7 @@ func play():
 
 
 func _ready():
-	color = Color(rand_range(0,1), rand_range(0,1), rand_range(0,1))
+	
 	load_fig()
 	init()
 	play()
